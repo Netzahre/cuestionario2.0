89ed3df913f1deb6a621d46c82138c9905b95725
Netzahre <netzahre@gmail.com>
2024-10-28 19:50:40 +0000

    wip 1

--- app/src/main/java/com/example/cuestionario_20/SQLiteHelper.kt
+++ app/src/main/java/com/example/cuestionario_20/SQLiteHelper.kt
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper
 class SQLiteHelper (contexto: Context, nombre: String, factory : SQLiteDatabase.CursorFactory?, id:Int) : SQLiteOpenHelper(contexto, nombre, factory, id) {
     override fun onCreate(db: SQLiteDatabase?) {
         if (db != null) {
-            db.execSQL("create table Usuarios(usuario text primary key, password text)")
+            db.execSQL("create table Usuarios(usuario text primary key, password text, notaMax real)")
         }
     }
 
